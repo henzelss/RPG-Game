cfedb4ac6cfe06f6a204c855479a6524ae526bfa
Henzel Quitaneg <110615201+henzelss@users.noreply.github.com>
2023-04-16 11:24:37 +0800

    Minor changes

--- script.js
+++ script.js
@@ -313,7 +313,7 @@ function pick(guess) {
     }
     text.innerText = "You picked " + guess + ". Here are the random numbers: \n";
     
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i <  10; i++){
         text.innerText += numbers[i] + "\n"; 
     }
 
